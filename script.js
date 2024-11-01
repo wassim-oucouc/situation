10d333a11f9aa37f04703de2c1937389d8458dd6
wassim <wassimoucouc2@gmail.com>
2024-11-01 12:32:24 +0100

    push

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question = "this is the first question",
+        answser = "this is the answer firstfor this question"
+    },
+
+    {
+        question ="this is the second question",
+        answer ="this is the second answer  "
+    },
+
+    {
+        question = "this is the tird question",
+        answer ="this is the tird answer"
+    },
+]
+
+
+
+const qplace = document.getElementById("changer");
+const rplace = document.getElementById("panelsStayOpen-collapseTwo");
+var index = 0;
+function textor()
+{
+
+fir.textContent =questions[index].question;
+li.textContent =questions[index].question;
+
+}
